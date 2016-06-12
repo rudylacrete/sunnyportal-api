10a8ce2757b9d4ca3ebd6dc778a5ed01ca5fe02b
Rudy GRONDIN <rudy.grondin@dsinstruments.fr>
2016-06-12 15:20:13 +0400

    modified function to make it works with new API and added method

--- test/test.js
+++ test/test.js
@@ -2,52 +2,45 @@
 var assert = require('assert');
 var SunnyPortal = require('../lib/sunnyportal')
 var opts = {
-	url : 'https://sunnyportal.com',
-	username : 'YOUR USERNAME',
-	password : 'YOUR PASSWORD',
-	plantOID : 'YOUR PLANT ID'
 	url : 'https://www.sunnyportal.com',
 }
+var plantOID = 'xxx-xxxxxxxx-xxxxxxx-xxxx';
+var fakePlantOID = 'aaa';
 
-var month = 4;
-var day = 4;
-var year = 2014;
+var sunnyPortal = new SunnyPortal(opts);
 
 //These are not proper unit tests but should be used as examples on how to access the API.
 describe('testSunnyPortalAPI',function() {
 	this.timeout(30000);
-	it('should return an integer value for current production', function(done) {
-		var sunnyPortal = new SunnyPortal(opts);
-		sunnyPortal.currentProduction(function(err, body) {
-			console.log(body);
-			assert(err ==null);
-			assert(body.PV >= 0);
-			done();
-		});
+
+	it('should return an error if the user is not logged in', function(done) {
+		sunnyPortal.productionData().then(done.bind(null, new Error('User is logged in but should not')))
+		.catch(done.bind(null, null));
 	});
 
-	it('should return data for a specific production date', function(done) {
-		var sunnyPortal = new SunnyPortal(opts);
-		sunnyPortal.historicalProduction(month,day,year,function(err, response) {
-			console.log(response);
-			assert(err == null);
-			assert(response.length=(24*4));
-			
-			for(var k in response) {
-				if(!isNaN(k)) {
-					var date = new Date(k*1000);
-					assert(month == date.getMonth());
-					//Check for the one rollover day.
-					assert(day == date.getDate() || (day+ 1) == date.getDate());
-					assert(year == date.getFullYear());
-					assert(response[k] >= 0 && response[k] <= 10000);
-				}
-			}
-			
-
-			done();
-		});
+	it('Should log in successfuly', function(done) {
+		sunnyPortal.login().then(done.bind(null, null), done);
 	});
 
+	it('should retrieve existing data once logged in', function(done) {
+		sunnyPortal.productionData().then(function(data) {
+			console.log(data);
+			assert(typeof data == "object", "Data is not an object");
+			done()
+		}).catch(done)
+	});
+
+	it('should retrieve data for one particular plant', function(done) {
+		sunnyPortal.productionDataForPlant(plantOID).then(function(data) {
+			console.log(data);
+			assert(data.hasOwnProperty('PlantName') == true, "Something wrong with the returned data");
+			done()
+		}).catch(done);
+	});
+
+	it('should return an error if the given plantOID does not exist', function(done) {
+		sunnyPortal.productionDataForPlant(fakePlantOID).then(done.bind(null, new Error("Get something but should not!")))
+		.catch(done.bind(null, null));
+	});
 
 });
